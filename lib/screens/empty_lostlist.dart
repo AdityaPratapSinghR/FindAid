c17007c5d310d4d7f1f199af36f26594270655e5
KartikeySamadhiya <kartikeys.ug23.cs@nitp.ac.in>
2024-04-06 22:24:35 +0530

    Initial page to display when no lost items match for a found item

---
+++ lib/screens/empty_lostlist.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class nosimilar_lost extends StatefulWidget {
+  const nosimilar_lost({super.key});
+
+  @override
+  State<nosimilar_lost> createState() => _nosimilar_lostState();
+}
+
+class _nosimilar_lostState extends State<nosimilar_lost> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width =  MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: const Color(0xffDCDCDCFF),
+          centerTitle: true,
+          title: const Text("empty similar lost item"),
+          actions: [
+      IconButton(
+      icon: const Icon(Icons.menu),
+      color: Colors.black,
+      onPressed:(){},
+    ),
+    ],
+    ),
+    body: SingleChildScrollView(
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Center(
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(20.0,20,20,0),
+        child: Image.asset('assets/images/images2.png',height: 300,width: 500,),
+      ),
+    ),
+      const SizedBox(height: 50,),
+      const Padding(
+        padding: EdgeInsets.fromLTRB(50,20,20,0),
+        child: Text('No similar lost items have been reported yet',
+          style: TextStyle(fontSize: 20.0),),
+      ),
+    ],
+    ),
+    ),
+    );
+  }
+}
