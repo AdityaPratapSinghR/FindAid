fdf773f64d2672b4624cecd7d6794d415939b464
KartikeySamadhiya <kartikeys.ug23.cs@nitp.ac.in>
2024-04-07 11:44:59 +0530

    Added the guidelines for lost items registration(Kartikey)

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
 
       routes: {
-        "/":(context)=> mainPage(),
+        "/": (context) => mainPage(),
 
       },
       theme: ThemeData(
@@ -53,8 +53,5 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5ffaea77a0809f956287e5cf448ef95e5a2278e6
+
