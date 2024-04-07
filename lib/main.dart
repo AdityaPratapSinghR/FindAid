57b54983bcf1f6bbebd0939095fd0d02cdc1be25
AdityaPratapSinghR <adityapratap.github@gmail.com>
2024-04-07 11:12:41 +0530

    Added Firebase function to add item to database(Adi)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:findaid/firebase_options.dart';
 import 'package:findaid/mainPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
@@ -24,6 +30,7 @@ class _MyAppState extends State<MyApp> {
 
       routes: {
         "/":(context)=> mainPage(),
+
       },
       theme: ThemeData(
         // This is the theme of your application.
