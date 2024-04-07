ae46b98bcc0f82c63252e02245f59dedc29deb5e
AdityaPratapSinghR <73656799+AdityaPratapSinghR@users.noreply.github.com>
2024-04-07 11:48:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
 
       routes: {
-        "/": (context) => mainPage(),
+        "/":(context)=> mainPage(),
 
       },
       theme: ThemeData(
